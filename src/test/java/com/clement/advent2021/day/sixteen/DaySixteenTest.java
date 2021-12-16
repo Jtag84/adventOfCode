14ca530e915ae80977f5e65b4d5c5e2955bdfe9d
Clement Vasseur <cvasseur@fundrise.com>
2021-12-16 09:33:59 -0500

    day 16

---
+++ src/test/java/com/clement/advent2021/day/sixteen/DaySixteenTest.java
@@ -0,0 +1,56 @@
+package com.clement.advent2021.day.sixteen;
+
+import static java.util.Collections.singletonList;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.collections4.ListUtils;
+import org.junit.jupiter.api.Test;
+
+import com.clement.utils.SolutionBase;
+
+class DaySixteenTest extends SolutionBase {
+	@Test
+	void part1() throws IOException {
+		Iterator<Long> bitIterator = getBitIterator();
+
+		Packet packet = Packet.decodePacket(bitIterator);
+
+		long summedVersions = getAllPackets(packet).stream().mapToLong(p -> p.version).sum();
+		log.info("answer Part1: " + summedVersions);
+	}
+
+	@Test
+	void part2() throws IOException {
+		Iterator<Long> bitIterator = getBitIterator();
+
+		Packet packet = Packet.decodePacket(bitIterator);
+
+		log.info("answer Part2: " + ((PacketOperator) packet).executeOperation());
+	}
+
+	private Iterator<Long> getBitIterator() throws IOException {
+		Iterator<Long> bitIterator = getInputsReader().readLine().chars()
+				.mapToLong(c -> Long.parseLong("1" + (char) c, 16))
+				.mapToObj(Long::toBinaryString)
+				.flatMapToLong(binary -> binary.chars().skip(1).asLongStream())
+				.map(b -> b - '0')
+				.iterator();
+		return bitIterator;
+	}
+
+	private List<Packet> getAllPackets(Packet packet) {
+		if (packet instanceof PacketOperator) {
+			List<Packet> subPackets = ((PacketOperator) packet).subPackets.stream()
+					.map(this::getAllPackets)
+					.flatMap(List::stream)
+					.toList();
+			return ListUtils.union(singletonList(packet), subPackets);
+		} else {
+			return singletonList(packet);
+		}
+	}
+
+}
---
+++ src/test/java/com/clement/advent2021/day/sixteen/PacketLiteral.java
@@ -0,0 +1,25 @@
+package com.clement.advent2021.day.sixteen;
+
+import java.util.Iterator;
+
+public class PacketLiteral extends Packet {
+
+	public final long value;
+
+	public PacketLiteral(long version, long typeId, long value, long length) {
+		super(version, typeId, length);
+		this.value = value;
+	}
+
+	public static PacketLiteral decodeLiteralPacket(long version, long typeId, Iterator<Long> bitIterator) {
+		long value = 0;
+		long length = 6;
+		long next5bits = 0;
+		do {
+			next5bits = getNextValueOfNbits(bitIterator, 5);
+			length += 5;
+			value = (value << 4) + (next5bits & 0b01111);
+		} while ((next5bits & 0b010000) > 0);
+		return new PacketLiteral(version, typeId, value, length);
+	}
+}
